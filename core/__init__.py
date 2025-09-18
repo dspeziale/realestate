b41e3371f76e79c2ea9a3d1218027b2f1c68c26f
Daniele.Speziale <dspeziale@gmail.com>
2025-09-18 08:55:49 +0200

    18/09 Mattina

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+# Questo file rende 'core' un package Python
+# È necessario per l'import: from core.traccar_framework import TraccarAPI
+
+__version__ = "1.0.0"
+__author__ = "Sistema Mappe Moderno"
